98b54423bfb7185ff7f2b1b262bd28b32b791a9e
cjs10lib <34670425+cjs10lib@users.noreply.github.com>
2019-01-20 04:47:51 +0000

    Created & design products page side-drawer

--- lib/pages/products/products_page.dart
+++ lib/pages/products/products_page.dart
@@ -47,6 +47,102 @@ class ProductsPage extends StatelessWidget {
     );
   }
 
+  Widget _buildDrawer({@required BuildContext context}) {
+    return Drawer(
+      child: Container(
+        color: Theme.of(context).backgroundColor,
+        child: Column(
+          children: <Widget>[
+            AppBar(
+              elevation: 0.0,
+              backgroundColor: Colors.transparent,
+              automaticallyImplyLeading: false,
+              title: Text(
+                'MENU',
+                style: TextStyle(
+                    color: _textColor,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.w900),
+              ),
+            ),
+            ClipRRect(
+                borderRadius: BorderRadius.circular(60.0),
+                child: Image.asset(
+                  'assets/images/temp4.jpg',
+                  fit: BoxFit.cover,
+                  height: 120.0,
+                  width: 120.0,
+                )),
+            SizedBox(height: 20.0),
+            Text('example@gmail.com',
+                style: TextStyle(
+                    color: _textColor,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold)),
+            FlatButton(
+              child: Text(
+                'View Profile',
+                style: TextStyle(
+                    color: Theme.of(context).primaryColor,
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold),
+              ),
+              onPressed: () {},
+            ),
+            Divider(height: 20.0),
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 20.0),
+              child: Column(
+                children: <Widget>[
+                  ListTile(
+                    leading: CircleAvatar(
+                        backgroundColor: _containerColor1,
+                        child: Icon(Icons.store_mall_directory,
+                            color: Theme.of(context).primaryColor)),
+                    title: Text(
+                      'Add Product',
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  ListTile(
+                    leading: CircleAvatar(
+                        backgroundColor: _containerColor1,
+                        child: Icon(Icons.graphic_eq,
+                            color: Theme.of(context).primaryColor)),
+                    title: Text(
+                      'Statistics',
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  Divider(height: 20.0),
+                   ListTile(
+                    leading: CircleAvatar(
+                        backgroundColor: _containerColor1,
+                        child: Icon(Icons.exit_to_app,
+                            color: Theme.of(context).primaryColor)),
+                    title: Text(
+                      'Signout',
+                      style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
   Widget _buildFavoriteToggle() {
     return IconButton(
       icon: Icon(
@@ -240,6 +336,7 @@ class ProductsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Theme.of(context).backgroundColor,
+        drawer: _buildDrawer(context: context),
         floatingActionButton: _buildShoppingCartFAB(context: context),
         body: CustomScrollView(
           slivers: <Widget>[
@@ -251,7 +348,8 @@ class ProductsPage extends StatelessWidget {
               // expandedHeight: 400.0,
               backgroundColor: Theme.of(context).backgroundColor,
               title: _buildSliverTitle(context),
-              leading: _buildDrawerToggle(),
+              automaticallyImplyLeading: true,
+              // leading: _buildDrawerToggle(),
               actions: <Widget>[
                 _buildFavoriteToggle(),
               ],
